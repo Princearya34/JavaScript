b145a399bdc979dec75e32ea98910d678c9d7e33
Princearya34 <109575282+Princearya34@users.noreply.github.com>
2023-09-18 17:02:44 +0000

    conversion of datatypes

---
+++ 01_basics/02_conversionOperation.js
@@ -0,0 +1,15 @@
+let score = null
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let booleanncheck =  ""
+let convertboolean = Boolean(booleanncheck)
+console.log(convertboolean)
+
+let rndnum = 34
+let str = String(rndnum)
+console.log(str)
+console.log(typeof str)
