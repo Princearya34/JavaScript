4e6f2cf3e99f5d2c58d3d9886cf0e4da445b96a4
Princearya34 <109575282+Princearya34@users.noreply.github.com>
2023-09-18 16:39:28 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,9 @@
+// number ==> 2 to power 53
+// bigint
+// string=> ""
+// boolean => true/false
+// null => standalone value
+// symbol => unique
+
+console.log(typeof undefined);//undefined
+console.log(typeof null); // object
