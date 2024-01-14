582cc3303ce238509947a0795e0d3aa9bc28a731
Princearya34 <109575282+Princearya34@users.noreply.github.com>
2024-01-14 18:24:02 +0000

    Object destructuring and JSON

---
+++ 02_basics/02_objectdestructuring.js
@@ -0,0 +1,9 @@
+const course ={
+    coursename:"Java DSA",
+    price: "999999",
+    courseInstructor:"Prince"
+}
+//course.courseInstructor
+
+const{courseInstructor}=course
+console.log(courseInstructor)
