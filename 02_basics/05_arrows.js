983d5a1ac1cf88982a1c8c431174ae78a7d19a3e
Princearya34 <109575282+Princearya34@users.noreply.github.com>
2024-01-15 18:08:52 +0000

    this and arrow function

---
+++ 02_basics/05_arrows.js
@@ -0,0 +1,19 @@
+//const user = {
+  //  username:"Prince",
+    //price:999,
+
+    //welcomeMessage: function(){
+      //  console.log(`${this.username},welcome to website`);
+
+    //}
+//}
+
+//const addTwo = (num1,num2) => {
+ //   return num1+num2
+//}
+//console.log(addTwo(3,4))
+
+//const addTwo = (num1,num2) =>  (num1+num2)
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+console.log(addTwo(3,4))
