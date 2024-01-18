0ab115c7846e21cffd876d4973e03208cea89488
Princearya34 <109575282+Princearya34@users.noreply.github.com>
2024-01-18 16:17:46 +0000

    for loop

---
+++ 04_iterations/01_one.js
@@ -0,0 +1,10 @@
+const array =[1,2,3,4,5];
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(element);
+}
+for(let i=1; i<=10; i++){
+    for(let j=1; j<=10; j++){
+        console.log(`${i}*${j}=${i*j}`)
+    }
+}
